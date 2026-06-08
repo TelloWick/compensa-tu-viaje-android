b18d318a8f5b6ec1e0ee645c652c80a33beffd12
TelloWick <fabriciovilca_2001@hotmail.com>
2026-06-08 00:37:27 -0500

    Integrar proveedor GPS en TrackingForegroundService

--- feature/location-service/src/main/kotlin/com/compensatuviaje/tracker/feature/locationservice/TrackingForegroundService.kt
+++ feature/location-service/src/main/kotlin/com/compensatuviaje/tracker/feature/locationservice/TrackingForegroundService.kt
@@ -3,11 +3,18 @@ package com.compensatuviaje.tracker.feature.locationservice
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class TrackingForegroundService : Service() {
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate() {
         super.onCreate()
+
+        fusedLocationClient =
+            LocationServices.getFusedLocationProviderClient(this)
     }
 
     override fun onStartCommand(

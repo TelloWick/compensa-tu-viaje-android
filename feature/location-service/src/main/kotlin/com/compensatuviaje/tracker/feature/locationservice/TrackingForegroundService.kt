d39923991a59dbba9a5a33346c696cc7f6e342ff
TelloWick <fabriciovilca_2001@hotmail.com>
2026-06-08 00:39:38 -0500

    Configurar LocationRequest de alta precisión

--- feature/location-service/src/main/kotlin/com/compensatuviaje/tracker/feature/locationservice/TrackingForegroundService.kt
+++ feature/location-service/src/main/kotlin/com/compensatuviaje/tracker/feature/locationservice/TrackingForegroundService.kt
@@ -5,16 +5,27 @@ import android.content.Intent
 import android.os.IBinder
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.Priority
 
 class TrackingForegroundService : Service() {
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
 
     override fun onCreate() {
         super.onCreate()
 
         fusedLocationClient =
             LocationServices.getFusedLocationProviderClient(this)
+
+        locationRequest =
+            LocationRequest.Builder(
+                Priority.PRIORITY_HIGH_ACCURACY,
+                5000L
+            )
+                .setMinUpdateIntervalMillis(3000L)
+                .build()
     }
 
     override fun onStartCommand(
